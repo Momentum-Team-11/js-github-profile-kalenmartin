3f3268ab3928354600dc0dff0e70a2a0ff8fd330
Kalen Martin <kalen.martin1@gmail.com>
2022-02-09 17:26:13 -0500

    riddle solved needs moar style

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 
 fetch('https://api.github.com/users/kalenmartin')
-    .then((response) => response.json())
+    .then((result) => result.json())
     .then((data) => {
-        const root = document.getElementById('root');
+
+//>result< NOT response! 
+
+
+        document.querySelector('#root').innerHTML +=
+
 //        console.log(root);
 //        console.log(root.innerHTML)
 //        root.innerHtml = `<p>${data.login}</p>`
 
-const username = document.createElement('h3')
-username.innerText = data.login
-root.appendChild(username)
 
 // const h3El = document.createElement('h3')
 // const h3Text = document.createTextNode("username: ")
@@ -17,30 +19,62 @@ root.appendChild(username)
 // root.appendChild(h3El)
 // console.log(h3El)
 
-let name = document.createElement('h3')
-name.innerText = data.name
-root.appendChild(name)
+    `<div class="header-img">
+        <img id="avatar" src=${data.avatar_url}>
+    </div>
+    <h2>${data.name}</h2>
+    <div class="profile">
+        <p>GitHub Username: ${data.login}</p>
+        <p>GitHub URL: <a href=${data.url}>${data.name}</a></p>
+        <p>Location: ${data.location}</p>            
+        <p>Following: ${data.following}</p>
+        <p>Followers: ${data.followers}</p>
+        <p>Website: <a href=${data.blog}>${data.name}</a></p>
+        </div>`
 
-let avatar_url = 
+    return data.repos_url
+})
 
-root.innerHTML = `
-<p class="name">${data.name}</p>
-<p class="username">@${data.login}</p>
-<p class="location">${data.location}</p>
-<img src=${"https://avatars.githubusercontent.com/u/98348889?v=4"} alt="Kalen avatar" class="center"/>
-<p class ="website">${"https://www.kalenmartin.com/"}</p>
-    <button>kalenmartin.com</button>
-<p class="followers">Followers: ${data.followers}</p>
-<p class="following">Following: ${data.following}</p>
-<p>GitHub Repos</p>
 
-<p class=repos> <a href="helloWorld${data.repos_url}/
-    <button>helloWorld</button>
-    <br>
-<p class=repos> <a href="patchwork"${data.repos_url}/
-    <button>patchwork</button>`
+.then((reposUrl) => fetch(reposUrl))
+.then((result) => result.json())
+.then((data) => {
 
-    })
+    console.log(data)
+    document.querySelector('#root').innerHTML +=
+        `<div id="repos">
+        <h3>GitHub Repos</h3>
+        </div>`
+
+    for(let repo of data) {
+        document.querySelector('#root').innerHTML +=
+        `<p><a href=${repo.url}>${repo.name}</a></p>`
+    }
+})
+
+
+
+
+
+
+
+// `<p class="name">${data.name}</p>
+// <p class="username">@${data.login}</p>
+// <p class="location">${data.location}</p>
+// <img src=${"https://avatars.githubusercontent.com/u/98348889?v=4"} alt="Kalen avatar" class="center"/>
+// <p class ="website">${"https://www.kalenmartin.com/"}</p>
+//     <button>kalenmartin.com</button>
+// <p class="followers">Followers: ${data.followers}</p>
+// <p class="following">Following: ${data.following}</p>
+// <p>GitHub Repos</p>
+
+// <p class=repos> <a href="helloWorld${data.repos_url}/
+//     <button>helloWorld</button>
+//     <br>
+// <p class=repos> <a href="patchwork"${data.repos_url}/
+//     <button>patchwork</button>`
+
+//     })
 
     
 
