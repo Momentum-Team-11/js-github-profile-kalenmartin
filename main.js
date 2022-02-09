e3080d4222cc766cc8f0364e73887bcc03813d84
Kalen Martin <kalen.martin1@gmail.com>
2022-02-09 09:09:25 -0500

    second day updates

--- main.js
+++ main.js
@@ -7,31 +7,45 @@ fetch('https://api.github.com/users/kalenmartin')
 //        console.log(root.innerHTML)
 //        root.innerHtml = `<p>${data.login}</p>`
 
-let username = document.createElement('h3')
+const username = document.createElement('h3')
 username.innerText = data.login
 root.appendChild(username)
+
+// const h3El = document.createElement('h3')
+// const h3Text = document.createTextNode("username: ")
+// h2El.appendChild(h3Text)
+// root.appendChild(h3El)
+// console.log(h3El)
 
 let name = document.createElement('h3')
 name.innerText = data.name
 root.appendChild(name)
 
 root.innerHTML = `
-<p class="name">${data.name}</p>
-<p class="username">${data.login}</p>
-<p class="location">${data.location}</p>
+<p class="name">Name: ${data.name}</p>
+<p class="username"> Username: ${data.login}</p>
+<p class="location">Location: ${data.location}</p>
 <img src=${data.avatar_url}/>
-<a href="helloWorld">${data.repos_url}/> <br>
-<a href="patchwork">${data.repos_url}/>`
-    })
-
-
-fetch("https://api.github.com/repos/kalenmartin/hello-world")
-    .then((response) => response.json())
-    .then((data) => {
-    const repos = document.getElementById('repos');
-        console.log(repos)
+<p class ="website">Website: ${data.blog}</p>
+<p class="followers">Followers: ${data.followers}</p>
+<p class="following">Following: ${data.following}</p>
+    <p class="GitHub Repos">GitHub Repos</p>
+<a href="helloWorld">${data.repos_url}/ 
+    <br>
+<a href="patchwork">${data.repos_url}/`
 
     })
+
+    
+
+
+// fetch("https://api.github.com/repos/kalenmartin/hello-world")
+//     .then((response) => response.json())
+//     .then((data) => {
+//     const repos = document.getElementById('repos');
+//         console.log(repos)
+
+//     })
 
 
 
